1e01f7a3abac9aa5cef4923f62f28b03f72e23b1
linoh <linoh@linkey1125.>
2025-07-12 23:42:06 +0900

    管理画面日本語化

--- ses_site/settings.py
+++ ses_site/settings.py
@@ -28,6 +28,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -97,9 +98,10 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # 国際化
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'ja'
+TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
+USE_L10N = True
 USE_TZ = True
 
 # Staticファイル設定
