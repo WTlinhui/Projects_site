c2d82cf97ff63ff0b7b65121d78cd272b3451a50
linkey1125 <lin.ohohoho@gmail.com>
2025-07-17 14:22:36 +0900

    ログエントリー追加

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Project
+from django.contrib.admin.models import LogEntry
 
 admin.site.register(Project)
+
+@admin.register(LogEntry)
+class LogEntryAdmin(admin.ModelAdmin):
+    list_display = ['action_time', 'user', 'content_type', 'object_repr', 'action_flag']
+    list_filter = ['action_flag', 'content_type', 'user']
+    search_fields = ['object_repr', 'change_message']
+    date_hierarchy = 'action_time'
